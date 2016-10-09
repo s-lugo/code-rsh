88b27c049893ddbe1d8077dd5ad7171ef389e32b
Sebastian Lugo <sebalugo@gmail.com>
2016-10-09 10:28:23 -0400

    Organizing project

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Normalize from 'normalize.css';
 import App from './App';
-import './index.css';
+
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
