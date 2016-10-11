729d07aba0c31d76930ec452ec4b635c3d3596b5
Sebastian Lugo <sebalugo@gmail.com>
2016-10-11 00:23:54 -0400

    Adding react-mdl dependency

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Normalize from 'normalize.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 import App from './App';
 
 
