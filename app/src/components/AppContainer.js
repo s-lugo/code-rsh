92ee47494d7e99fde0270e0bb12da1a82c762c1a
Sebastian Lugo <sebalugo@gmail.com>
2016-10-11 23:54:25 -0400

    Container components for better routing

---
+++ app/src/components/AppContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppContainer = (props) => (
+    <div>
+        {props.children}
+    </div>
+);
+
+export default AppContainer;
--- app/src/components/Container.js
+++ app/src/components/Container.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import Footer from './Footer.js';
 
 const Container = (props) => (
 	<div>
 		<NavBar />
 		{props.children}
+		<Footer />
 	</div>
 );
 
