5eff713808a1e751b256090a2b2dc01391c66e28
Sebastian Lugo <sebalugo@gmail.com>
2016-10-12 00:09:51 -0400

    Landing page design

--- app/src/pages/Landing.js
+++ app/src/pages/Landing.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Layout, Header, Navigation, Content, Button } from 'react-mdl';
+import { Link } from 'react-router';
+import backgroundImage from '../assets/img/landing-background.jpg';
 
 const Landing = () => (
     <div>
+        <div style={{height: '400px', position: 'relative'}}>
+            <Layout style={{background: `url(${backgroundImage}) center / cover`}}>
+                <Header transparent title="Title" style={{color: 'orange'}}>
+                    <Navigation >
+                        <Link to="#">Log In</Link>
+                        <Link to="#">Sign In</Link>
+                    </Navigation>
+                </Header>
+                <Content style={{textAlign:'center'}}>
+                    <Button raised accent ripple>Button</Button>
+                </Content>
+            </Layout>
+        </div>
     </div>
 );
 
