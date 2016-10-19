4267db73fcb497607c2cd289f831fb18e89269e5
Sebastian Lugo <sebalugo@gmail.com>
2016-10-19 19:42:06 -0400

    Organizing languages components and containers

---
+++ app/src/components/LanguagesList.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const LanguagesList = ({languages}) => {
+
+};
+
+export default LanguagesList;
+
+
---
+++ app/src/components/LanguagesPreview.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Grid, Cell, Card, CardTitle, CardActions } from 'react-mdl';
+
+const styles = {
+    language: {
+       
+    },
+}
+
+const renderLanguages = ( arr ) => {
+    return arr.map( (language) =>(
+        <Cell col={3} style={styles.language} >
+
+        </Cell>
+    ));
+};
+
+const AvailableLanguages = ({ languages }) => (
+    <div style={{width: '70%', margin: '0 auto', padding: '80px 0px'}}>
+        <Grid>
+            <Cell col={12} style={{textAlign: 'center'}} >
+                <h2> Practice in your favorite languages </h2>
+            </Cell>
+            {renderLanguages(languages)}
+        </Grid>
+    </div>
+);
+
+export default AvailableLanguages;
+
+
