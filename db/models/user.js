2ca6408c2adcb75eca0019a7eff427093b03326d
Sebastian Lugo <sebalugo@gmail.com>
2016-10-26 01:15:54 -0400

    Designing user schema

--- db/models/user.js
+++ db/models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const userSchema = mongoose.Schema({
 	name: String,
 	achivements: [
 		{
-			_id: mongoose.Schema.Types.ObjectId,
+			_id: ObjectId,
 			achieved: Date,
 		},
 	],
 	matches: [
-		
+		{
+			score: Number,
+			wpm: Number,
+			time: Number,
+			course: ObjectId,
+			opponent: String,
+		},
 	],
 });
 
