d587fa6d1001ce91745fd8749dd0c81dd65d8777
Sebastian Lugo <sebalugo@gmail.com>
2016-10-28 23:28:08 -0400

    Refactoring db initializer and seed

--- db/initialize.js
+++ db/initialize.js
@@ -4,41 +4,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const initialData = require('./seed.json');
 
-
 mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
-const User = db.model('User', schemas.user);
-const Language = db.model('Language', schemas.language);
-const Achievement = db.model('Achievement', schemas.achievement);
-
-initialData.achievements.forEach((achievement)=>{
-    const { name, description, image } = achievement;
-    Achievement.create({
-        name,
-        description,
-        image
-    })
-    .then((language,err)=>{
-        if(err)console.log(err);
-        console.log("SAVED");
-    });
-});
-
-initialData.languages.forEach((language)=>{
-    const { name, description, courses } = language;
-    Language.create({
-        name,
-        description,
-        courses
-    })
-    .then((language,err)=>{
-        if(err)console.log(err);
-        console.log("SAVED");
-    });
-});
-
-
-
-
-
+for (let key in initialData) {
+	const model = db.model(key, schemas[key]);
+	for (let data of initialData[key]) {
+		model.create({...data})
+			.then((record)=>console.log(`Saved ${record}`))
+			.catch((err)=>console.log(err));
+	} 
+}
