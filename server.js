db1f7d07d19ec010ca240a1bf6a2c3865a124f0a
Sebastian Lugo <sebalugo@gmail.com>
2016-11-04 01:06:59 -0400

    Socket.io initial server config

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const bodyParser = require('koa-body');
 const logger = require('koa-logger');
 const mongoose = require('mongoose');
 const router = require('./api/routes/index');
-const koa = require('koa');
+const app = require('koa')();
 
 require('dotenv').config();
 mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
-const app = koa();
 
 app.use(logger());
 app.use(bodyParser());
 app.use(router);
 
-app.listen(3001);
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server);
+
+io.on('connection', function(){console.log("IOIO") });
+server.listen(3001);
