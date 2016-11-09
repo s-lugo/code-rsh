7d957225dec5fb97d649d4f28739b94a03f0fd74
Sebastian Lugo <sebalugo@gmail.com>
2016-11-08 20:04:34 -0400

    Adding redux store and config

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import routes from './routes/routes.js';
+import store from './store/store.js';
 
-class App extends Component {
-	render() {
+const App = () => {
 		return (
-			<Router history={browserHistory}>
-				{routes}
-			</Router>
+			<Provider store={store} >
+				<Router history={browserHistory}>
+					{routes}
+				</Router>
+			</Provider>
 		);
-	}
-}
+};
 
 export default App;
---
+++ app/src/store/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import FastApp from '../reducers/rootReducer.js';
+
+const store = createStore(
+	FastApp,
+	applyMiddleware(ReduxThunk)
+);
+
+export default store;
