e15ec9ef820a189bf6b2ca3c1fa79e93fe9e9e4d
Sebastian Lugo <sebalugo@gmail.com>
2016-11-10 00:45:04 -0400

    Minor layout fix

--- app/src/pages/Practice.js
+++ app/src/pages/Practice.js
@@ -4,7 +4,6 @@ import AvailableLanguages from '../containers/AvailableLanguages.js';
 const Practice = () => {
     return (
     <div style={{margin: '0 auto', padding: '40px 0px', textAlign: 'center'}}>
-        <h2> Select your language </h2>
         <AvailableLanguages />
     </div>
     );
