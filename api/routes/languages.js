87375337de05084dd41564098a31c6571519cf71
Sebastian Lugo <sebalugo@gmail.com>
2016-11-11 23:53:51 -0400

    Adding simulateLatency function api test

--- api/routes/languages.js
+++ api/routes/languages.js
@@ -4,12 +4,17 @@ const languageSchema = require('../../db/models/language');
 
 const Language = mongoose.model('Language', languageSchema);
 
+function simulateLatency(){
+    return new Promise((resolve,reject) => setTimeout(resolve,2000));
+}
+
 router
-    .get('/api/languages', function* (){
+    .get('/api/languages', function* () {
+        //yield simulateLatency();
         const languages = yield Language.find({}, 'name courses -_id');
         this.response.body = languages;
     })
-    .get('/api/languages/:id', function* (){
+    .get('/api/languages/:id', function* () {
         const language = yield Language.find({});
         this.body = language;
     });
