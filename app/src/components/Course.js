906361afba4c91c8b17c46cac9f8c9d99597d203
Sebastian Lugo <sebalugo@gmail.com>
2016-11-11 23:54:16 -0400

    Courses components

--- app/src/components/Course.js
+++ app/src/components/Course.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Course = (props) => {
-
+    return <div></div>;
 };
 
 export default Course;
--- app/src/components/CoursesList.js
+++ app/src/components/CoursesList.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Course from './Course.js';
+
+function renderCourses(courses, selectCourse){
+    return courses.map( (course) =>{
+        <Cell col={4}>
+            <Course {...course} selectCourse={selectCourse} />
+        </Cell>
+    });
+}
 
 const CoursesList = (props) => {
-    return <div></div>;
+    if( !props.languages.length ){
+        return <div></div>;
+    }
+    const courses = props.languages.filter((lang) => lang.name == props.selectedLanguage)[0].courses;
+    return (
+    <div style={{ width: '70%', margin: '0 auto' }}>
+            <Grid>
+                {renderCourses(props.languages, props.selectLanguage)}
+            </Grid>
+    </div>);
 };
 
 export default CoursesList;
