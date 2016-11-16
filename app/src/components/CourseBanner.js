b989864c84e32c80bd0de0a2d37d002e80150e25
Sebastian Lugo <sebalugo@gmail.com>
2016-11-15 23:56:17 -0400

    Adding CourseBanner component

---
+++ app/src/components/CourseBanner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Card, CardTitle, CardText, CardActions, CardMenu, Button } from 'react-mdl';
+
+const CourseBanner = (props) => {
+    return <div></div>;
+};
+
+export default CourseBanner;
