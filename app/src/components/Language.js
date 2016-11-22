db068f03e28db96f325d3b3880895ecd48b85ff4
Sebastian Lugo <sebalugo@gmail.com>
2016-11-21 22:27:17 -0400

    Minor layout changes to language component

--- app/src/components/Language.js
+++ app/src/components/Language.js
@@ -19,21 +19,32 @@ let styles = {
     },
 }
 
+const backgrounds = {
+    Ruby: '#B71C1C',
+    Javascript: '#FBC02D',
+    Java: '#512DA8',
+    Haskell: '#607D8B',
+    Python: '#1976D2',
+    PHP:'#5C6BC0',
+}
+
 const Language = (props) => {
     return (
-    <Card shadow={1} style={{width: '80%', margin: 'auto'}}>
-        <div>
-            <img style={styles.logo} src={Logos[`${props.name}-logo`]} />
-        </div>
-        <CardText>
-            <span style={styles.span}>{`0/${props.courses.length} Courses Completed`}</span>
-            <ProgressBar progress={44} />
-        </CardText>
-        <CardActions border>
-            <Button colored onClick={()=>props.selectLanguage(props.name)}><Link to={`/practice/${props.name}`} style={styles.link}>Select {props.name}</Link></Button>
-        </CardActions>
-    </Card> 
-);
+        <Card shadow={1} style={{width: '80%', margin: 'auto'}}>
+            <CardTitle expand style={{color: '#fff', backgroundColor: backgrounds[props.name] }}>
+                <div style={{margin: '0 auto'}}>
+                    <img style={styles.logo} src={Logos[`${props.name}-logo`]} />
+                </div>
+            </CardTitle>
+            <CardActions border>
+                <Button colored onClick={()=>props.selectLanguage(props.name)}><Link to={`/practice/${props.name}`} style={styles.link}>{props.name}</Link></Button>
+            </CardActions>
+            <CardText>
+                <span style={styles.span}>{`0/${props.courses.length} Courses Completed`}</span>
+                <ProgressBar progress={44} />
+            </CardText>
+        </Card> 
+    );
 }
 
 export default Language;
