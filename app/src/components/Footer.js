1381117a774716738751f502617babbd0c5206f3
Sebastian Lugo <sebalugo@gmail.com>
2016-11-24 21:33:26 -0400

    Make footer stay bottom of page

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Footer, FooterSection } from 'react-mdl';
 
+let styles = {
+    footer:{
+        position: 'absolute',
+        bottom: '0px',
+        left: '0px',
+        width: '100%',
+        paddingLeft: 0,
+        paddingRight: 0,
+    },
+}
+
 export default () => (
-    <Footer size="mini">
+    <Footer style={styles.footer} size="mini">
         <FooterSection type="left" logo="© 2016 Sebastian Lugo.">
         </FooterSection>
     </Footer>
